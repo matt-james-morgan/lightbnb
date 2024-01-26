02bde78fd1c6de7a61542a6c101b39c965811c4f
matt-james-morgan <matt.james.morgan97@gmail.com>
2024-01-25 20:26:43 -0500

    adds functionality to search feature

--- LightBnB_WebApp-master/db/database.js
+++ LightBnB_WebApp-master/db/database.js
@@ -104,9 +104,51 @@ const getAllReservations = function (guest_id, limit = 10) {
  */
 
 const getAllProperties = (options, limit = 10) => {
+  const queryParams = [];
+
+  let queryString = `
+  SELECT properties.*, avg(property_reviews.rating) as average_rating
+  FROM properties
+  JOIN property_reviews ON properties.id = property_id
+  `;
+
+  if(options.city){
+    queryParams.push(`%${options.city}%`);
+    queryString += ` WHERE city LIKE $${queryParams.length}`;
+  }
+
+  if(options.minimum_price_per_night){
+
+    queryParams.push(`${options.minimum_price_per_night * 100 }`);
+    queryString += ` AND properties.cost_per_night > $${queryParams.length}`;
+    
+  }
+
+  if(options.maximum_price_per_night){
+    queryParams.push(`${options.maximum_price_per_night * 100}`);
+    queryString += ` AND properties.cost_per_night < $${queryParams.length}`;
+  }
+  
+  queryString += `GROUP BY properties.id`;
+
+  if(options.minimum_rating){
+    queryParams.push(`${options.minimum_rating}`);
+    queryString += ` HAVING AVG(property_reviews.rating) > $${queryParams.length}`;
+  }
+
+
+  queryParams.push(limit);
+
+  queryString += `
+  ORDER BY cost_per_night
+  LIMIT $${queryParams.length};
+  `;
+  console.log(queryString);
   return pool
-    .query(`SELECT * FROM properties LIMIT $1`, [limit])
+    .query(queryString, queryParams)
     .then((result) => {
+      console.log(queryString);
+     
       return result.rows;
     })
     .catch((err) => {
